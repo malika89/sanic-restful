9afafec9f537b839b65e7221d0d5e5c2b3b82077
macy <xx>
2021-09-02 00:19:16 +0800

    rename_cloudapi
    基础cloud client

--- apps/articles/router.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# coding:utf-8
-
-
-from sanic import Blueprint
-from .views import ArticleView
-
-router = Blueprint("articles",url_prefix="/article")
-router.add_route(ArticleView.as_view(),"/")
--- apps/articles/views.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from common.base.base_view import BaseView
-from .models import Articles
-
-class ArticleView(BaseView):
-    Model = Articles
